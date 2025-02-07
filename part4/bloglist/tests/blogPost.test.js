f9066d4a263e60ec446921c5fb12805bbc9227ec
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-02-07 20:49:28 +0100

    Exercise 4.6 and 4.7

--- part4/bloglist/tests/blogPost.test.js
+++ part4/bloglist/tests/blogPost.test.js
@@ -55,6 +55,57 @@ const blogs = [
     }
 ]
 
+const blogs2 = [
+    {
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Edsger W. Dijkstra",
+        url: "https://reactpatterns.com/",
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: "5a422b3a1b54a676234d17f9",
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: "5a422b891b54a676234d17fa",
+        title: "First class tests",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: "5a422ba71b54a676234d17fb",
+        title: "TDD harms architecture",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: "5a422bc61b54a676234d17fc",
+        title: "Type wars",
+        author: "Robert C. Martin",
+        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+        likes: 2,
+        __v: 0
+    }
+]
+
 const listWithOneBlog = [{
     _id: '5a422aa71b54a676234d17f8',
     title: 'Go To Statement Considered Harmful',
@@ -94,7 +145,31 @@ describe('favorite blog', () => {
 })
 
 describe('most blogs', () => {
+    test('of an empty list', () => {
+        deepEquals(listHelper.mostBlogs([]), null)
+    })
+    test('of a single blog list', () => {
+        deepEquals(listHelper.mostBlogs(listWithOneBlog), {author: 'Edsger W. Dijkstra', blogs: 1})
+    })
     test('of a full list', () => {
         deepEquals(listHelper.mostBlogs(blogs), {author: "Robert C. Martin", blogs: 3})
+    })
+    test('of a full list with a match', () => {
+        deepEquals(listHelper.mostBlogs(blogs2), {author: "Edsger W. Dijkstra", blogs: 3})
+    })
+})
+
+describe('author with most likes', () => {
+    test('of an empty list', () => {
+        deepEquals(listHelper.mostLikes([]), null)
+    })
+    test('of a single blog list', () => {
+        deepEquals(listHelper.mostLikes(listWithOneBlog), {author: 'Edsger W. Dijkstra', likes: 5})
+    })
+    test('of a full list', () => {
+        deepEquals(listHelper.mostLikes(blogs), {author: "Edsger W. Dijkstra", likes: 17})
+    })
+    test('of a full list with a match', () => {
+        deepEquals(listHelper.mostLikes(blogs2), {author: "Edsger W. Dijkstra", likes: 17})
     })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,4 +1,5 @@
 const {log, error} = require("../utils/logger")
+const lodash = require("lodash");
 const dummy = (blogs) => 1
 const totalLikes = (blogs) =>  blogs.reduce((acc, val) => acc + val.likes, 0);
 
@@ -14,6 +15,43 @@ const favoriteBlog = (blogs) => {
 }
 
 const mostBlogs = (blogs) => {
+    if (!blogs || blogs.length === 0)
+    {
+        return null;
+    }
+    const max = lodash.groupBy(blogs, 'author');
+
+    let author = { author: null, blogs: 0 };
+
+    lodash.forEach(max, (authorBlogs, _author) =>{
+        const length = authorBlogs.length;
+        if (author.blogs < length)
+        {
+            author = {author: _author, blogs: length};
+        }
+    })
+    return author
+}
+
+const mostLikes = (blogs) => {
+    if (!blogs || blogs.length === 0)
+    {
+        return null;
+    }
+    const authors = lodash.groupBy(blogs, 'author');
+    let author = {author: null, likes:0};
+
+    lodash.forEach(authors, (authorBlogs, _author) => {
+        const sum = authorBlogs.reduce((acc, blog) => acc + blog.likes, 0);
+        if (sum > author.likes)
+        {
+            author = { author:_author, likes: sum }
+        }
+    })
+    return author;
+}
+
+const mostBlogsManual = (blogs) => {
     let authors = [];
     blogs.forEach(b => {
         let existing = authors.find(blog => blog.author === b.author);
@@ -39,4 +77,4 @@ const mostBlogs = (blogs) => {
     return mostBlogged;
 }
 
-module.exports = {dummy, totalLikes, favoriteBlog, mostBlogs};
+module.exports = {dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes};
