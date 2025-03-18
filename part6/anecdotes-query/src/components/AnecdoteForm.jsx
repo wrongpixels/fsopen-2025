aa30b872deb798090712b9b9964d9de141db7a44
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-03-18 19:36:55 +0100

    Exercise 6.21

--- part6/anecdotes-query/src/components/AnecdoteForm.jsx
+++ part6/anecdotes-query/src/components/AnecdoteForm.jsx
@@ -1,21 +1,36 @@
+import { addAnecdote } from '../requests.js'
+import { useQueryClient, useMutation } from '@tanstack/react-query'
+
 const AnecdoteForm = () => {
 
-  const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    const queryClient = useQueryClient()
+    const getCurrentAnecdotes = () => queryClient.getQueryData(['anecdotes'])
 
-  return (
-    <div>
-      <h3>create new</h3>
-      <form onSubmit={onCreate}>
-        <input name='anecdote' />
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
+    const addAnecdoteMutation = useMutation({
+        mutationKey:'anecdotes',
+        mutationFn: addAnecdote,
+        onSuccess: (newAnecdote) => {
+            queryClient.setQueryData(['anecdotes'], getCurrentAnecdotes()
+                .concat(newAnecdote))
+        }
+    })
+
+    const onCreate = (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        addAnecdoteMutation.mutate({content, votes: 0})
+        event.target.anecdote.value = ''
+    }
+
+    return (
+        <div>
+            <h3>create new</h3>
+            <form onSubmit={onCreate}>
+                <input name='anecdote'/>
+                <button type="submit">create</button>
+            </form>
+        </div>
+    )
 }
 
 export default AnecdoteForm
--- part6/anecdotes-query/src/requests.js
+++ part6/anecdotes-query/src/requests.js
@@ -6,3 +6,8 @@ export const getAnecdotes = async () => {
     const response = await axios.get(`${URL}/anecdotes`)
     return response.data
 }
+
+export const addAnecdote = async (anecdote) => {
+    const response = await axios.post(`${URL}/anecdotes`, anecdote)
+    return response.data
+}
