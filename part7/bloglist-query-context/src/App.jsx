98598dcd471dfc5daede38ed26a8f421139140c7
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-03-24 19:28:44 +0100

    Before 7.10-13 exercises

---
+++ part7/bloglist-query-context/src/App.jsx
@@ -0,0 +1,169 @@
+import { useState, useEffect, useRef } from 'react'
+import Blog from './components/Blog'
+import NewBlog from './components/NewBlog.jsx'
+import Notification from './components/Notification'
+import LoginForm from './components/LoginForm.jsx'
+import Toggleable from './components/Toggleable.jsx'
+import blogService from './services/blogs'
+import blogServices from './services/blogs.js'
+
+const USER_KEY = 'activeUser'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+
+  const [notification, setNotification] = useState({ message:'', error:true })
+  const newBlogRef = useRef()
+  const loginFormRef = useRef()
+
+  const showError = (message) => sendNotification(message, true)
+  const showNotification = (message) => sendNotification(message, false)
+
+
+  const sendNotification = (message, error = true) => {
+    if(!message)
+    {
+      return
+    }
+    setNotification({ message, error })
+    setTimeout(() => {
+      setNotification({ message: '', error: true })
+
+    }, 5000)
+  }
+
+
+  useEffect(() => {
+    if (!user)
+    {
+      const existingSession = window.localStorage.getItem(USER_KEY)
+      if (existingSession)
+      {
+        const validUser = JSON.parse(existingSession)
+        if (validUser && validUser.token)
+        {
+          setUser(validUser)
+        }
+        else
+        {
+          window.localStorage.removeItem(USER_KEY)
+        }
+      }
+    }
+  }, [])
+  useEffect(() => {
+    if (user) {
+      getAllBlogs()
+      blogService.buildToken(user.token)
+    }
+    else
+    {
+      blogService.buildToken('')
+    }
+
+  }, [user])
+
+  const addNewBlog = async (title, author, url) => {
+    const newBlog = await blogServices.addBlog(title, author, url)
+    if (newBlog && newBlog.title === title)
+    {
+      setBlogs(blogs.concat(newBlog))
+      showNotification(`'${title}' by ${author} was added to the Blog List!`, false)
+    }
+    else if (newBlog.error)
+    {
+      showNotification(newBlog.error)
+    }
+    else
+    {
+      showNotification('There was an error adding the entry')
+    }
+    return newBlog
+  }
+
+  const deleteBlog = (id) => orderBlogs(blogs.filter(b => b.id !== id))
+
+  const addLike = async (blog) => {
+    const editedBlog = await blogService.replaceBlogData({ likes: blog.likes + 1 }, blog.id, showNotification)
+    if (editedBlog)
+    {
+      showNotification('Blog was liked!')
+      replaceBlog(editedBlog)
+    }
+  }
+  const replaceBlog = (editedBlog) => {
+    const updatedBlogs = blogs.map(b => b.id === editedBlog.id?editedBlog:b)
+    orderBlogs(updatedBlogs)
+  }
+
+  const getAllBlogs = async () => {
+    const allBlogs = await blogService.getAll()
+    orderBlogs(allBlogs)
+  }
+  const orderBlogs = (targetBlogs = blogs) => {
+    if (!targetBlogs)
+    {
+      return
+    }
+    setBlogs([...targetBlogs].sort((a, b) => b.likes - a.likes))
+  }
+  const setSession = (userData) => {
+    setUser(userData)
+    showNotification(`Welcome back, ${userData.name}!`)
+    window.localStorage.setItem(USER_KEY, JSON.stringify(userData))
+  }
+  const doLogOut = () => {
+    showNotification(`See you soon, ${user.name}!`)
+    setUser(null)
+    window.localStorage.removeItem(USER_KEY)
+    loginFormRef.current?.cleanForm()
+  }
+  const loginForm = () => <><LoginForm
+    showError={showError}
+    setSession={setSession}
+    ref={loginFormRef}
+  /></>
+
+  const drawBlogs = () => (
+    <div className="blog-list">
+      <h2>Blogs</h2>
+      <p>
+              Logged in as <b> {user.username} </b><button onClick={doLogOut}>Log out</button>
+      </p>
+      {blogs.map(blog =>
+        <Blog key={blog.id}
+          blog={blog}
+          showNotification={sendNotification}
+          likeBlog={addLike}
+          activeUser={user}
+          deleteBlog={deleteBlog}
+        />
+      )}
+      <div>
+        <Toggleable
+
+          ref={newBlogRef}
+          labelOnVisible={'Hide new Blog Form'}
+          labelOnInvisible={'Add a new Blog'}
+          initialVisibility={false}
+          addSpace={false}
+          showOver={true}
+        >
+          <NewBlog
+            showNotification={sendNotification}
+            addNewBlog={addNewBlog}
+          />
+        </Toggleable></div>
+    </div>
+  )
+
+  return (
+    <>
+      <Notification notification={notification} />
+      {user?drawBlogs():loginForm()}
+    </>
+  )
+}
+
+export default App
---
+++ part7/bloglist-query-context/src/components/Notification.jsx
@@ -0,0 +1,25 @@
+const Notification = ({ notification }) => {
+  const { message, error } = notification
+
+  if (!message) {
+    return null
+  }
+
+  const notificationStyle = {
+    color: error ? 'red' : 'green',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 6,
+    padding: 10,
+    margin: 10
+  }
+
+  return (
+    <div style={notificationStyle} className="notification">
+      {message}
+    </div>
+  )
+}
+
+export default Notification
