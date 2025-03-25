57619b580c77851de8e1f45b2cbab9baa3617ae5
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-03-25 16:08:06 +0100

    Part 5 - Typo

--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,47 +1,53 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from "axios";
+const baseUrl = "/api/blogs";
 
-let activeToken = {}
+let activeToken = {};
 
-const buildToken = (token) => activeToken = { authorization: `Bearer ${token}` }
+const buildToken = (token) =>
+  (activeToken = { authorization: `Bearer ${token}` });
 
 const getAll = async () => {
-  const request = await axios.get(baseUrl)
-  return request.data
-}
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
 const replaceBlogData = async (data, id, showNotification) => {
-  const response = await axios.put(`${baseUrl}/${id}`, data)
-  if (response.data)
-  {
-    return response.data
+  const response = await axios.put(`${baseUrl}/${id}`, data);
+  if (response.data) {
+    return response.data;
+  } else {
+    showNotification("Error updating blog info");
   }
-  else
-  {
-    showNotification('Error updating blog info')
-  }
-}
+};
 
 const addBlog = async (title, author, url) => {
-  if (!activeToken || !activeToken.authorization || activeToken.authorization === 'Bearer ')
-  {
-    return { error: 'Token is not valid' }
+  if (
+    !activeToken ||
+    !activeToken.authorization ||
+    activeToken.authorization === "Bearer "
+  ) {
+    return { error: "Token is not valid" };
   }
   try {
-    const addedBlog = (await axios.post(baseUrl, { title, author, url }, { headers: activeToken }))
-    return addedBlog.data
-  }catch (e){
-    return e.response.data
+    const addedBlog = await axios.post(
+      baseUrl,
+      { title, author, url },
+      { headers: activeToken },
+    );
+    return addedBlog.data;
+  } catch (e) {
+    return e.response.data;
   }
-}
+};
 
 const deleteBlog = async (id) => {
-  try
-  {
-    const response = await axios.delete(`${baseUrl}/${id}`, { headers: activeToken })
-    return response.data
-  }catch (e){
-    return e.response.data
+  try {
+    const response = await axios.delete(`${baseUrl}/${id}`, {
+      headers: activeToken,
+    });
+    return response.data;
+  } catch (e) {
+    return e.response.data;
   }
-}
+};
 
-export default { getAll, addBlog, replaceBlogData, deleteBlog, buildToken }
+export default { getAll, addBlog, replaceBlogData, deleteBlog, buildToken };
