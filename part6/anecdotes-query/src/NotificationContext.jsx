4e8be9e9a66634fc5b46ff2e4b7a2a54cca3f581
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-03-31 14:47:12 +0200

    Revert Part 6 wrongly replaced NotificationContext

--- part6/anecdotes-query/src/NotificationContext.jsx
+++ part6/anecdotes-query/src/NotificationContext.jsx
@@ -1,14 +1,13 @@
 import { createContext, useReducer, useContext } from "react";
 
 const NotificationContext = createContext();
-const initialState = { message: "", error: false };
 
 const notificationReducer = (state, { type, payload }) => {
   switch (type) {
-    case "NOTIFICATION_SET":
+    case "SET_NOTIFICATION":
       return payload;
-    case "NOTIFICATION_RESET":
-      return initialState;
+    case "RESET_NOTIFICATION":
+      return "";
     default:
       return state;
   }
@@ -18,7 +17,7 @@ export const useValue = () => useContext(NotificationContext)[0];
 export const useDispatch = () => useContext(NotificationContext)[1];
 
 export const NotificationContextProvider = (props) => {
-  const providerValues = useReducer(notificationReducer, initialState);
+  const providerValues = useReducer(notificationReducer, "");
   return (
     <NotificationContext.Provider value={providerValues}>
       {props.children}
