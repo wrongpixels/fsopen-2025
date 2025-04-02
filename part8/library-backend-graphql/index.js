30849d098fb86869230d8469cef108f9edd5f4e6
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-04-02 12:49:34 +0200

    Exercises 8.1, 8.2 and 8.3

--- part8/library-backend-graphql/index.js
+++ part8/library-backend-graphql/index.js
@@ -27,20 +27,6 @@ let authors = [
   },
 ]
 
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conexión con el libro
- */
-
 let books = [
   {
     title: 'Clean Code',
@@ -93,19 +79,49 @@ let books = [
   },
 ]
 
-/*
-  you can remove the placeholder query once your first one has been implemented
-*/
+const typeDefs = /* GraphQL */ `
+  enum Genre {
+    classic
+    revolution
+    crime
+    design
+    refactoring
+    patterns
+    agile
+  }
 
-const typeDefs = `
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID
+    genres: [Genre!]!
+  }
+
+  type Author {
+    name: String!
+    id: ID
+    born: Int
+    bookCount: Int!
+  }
   type Query {
-    dummy: Int
+    bookCount: Int!
+    authorCount: Int!
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
 const resolvers = {
   Query: {
-    dummy: () => 0,
+    bookCount: () => (books ? books.length : 0),
+    authorCount: () => (authors ? authors.length : 0),
+    allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) =>
+      books ? books.filter((b) => b.author === root.name).length : 0,
   },
 }
 
