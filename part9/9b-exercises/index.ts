7da73a463020bc0db689a23b41eb60ba461ac742
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-04-10 17:09:15 +0200

    Exercise 9.4

---
+++ part9/9b-exercises/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+
+app.get('/hello', (_req, res) => {
+  res.send('Hello, Full Stack!');
+});
+
+const PORT: number = 3003;
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
