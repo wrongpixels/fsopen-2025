c5854900dc2ad097124949b3b4ab85f15465e156
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-04-10 12:35:13 +0200

    Exercise 9.1 [Prettier]

--- part9/9b-exercises/bmiCalculator.ts
+++ part9/9b-exercises/bmiCalculator.ts
@@ -1,28 +1,28 @@
-type BmiRange = 'Underweight' | 'Normal' | 'Overweight' | 'Obese'
+type BmiRange = 'Underweight' | 'Normal' | 'Overweight' | 'Obese';
 
 const getBmiRange = (result: number): BmiRange => {
-  console.log(result)
+  console.log(result);
   if (result < 18.5) {
-    return 'Underweight'
+    return 'Underweight';
   }
   if (result >= 30) {
-    return 'Obese'
+    return 'Obese';
   }
   if (result > 25) {
-    return 'Overweight'
+    return 'Overweight';
   }
-  return 'Normal'
-}
+  return 'Normal';
+};
 
 const calculateBmi = (height: number, weight: number): string => {
-  const result: number = weight / (height / 100) ** 2
-  return `${getBmiRange(result)} range`
-}
+  const result: number = weight / (height / 100) ** 2;
+  return `${getBmiRange(result)} range`;
+};
 
 try {
-  console.log(calculateBmi(180, 74))
+  console.log(calculateBmi(180, 74));
 } catch (e) {
   console.log(
     e instanceof Error ? `Error: ${e.message}` : 'Error: Something went wrong!'
-  )
+  );
 }
