f3b612c59c2c267053b610f348dd0066dcaefcde
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-05-04 22:40:21 +0200

    Exercise 12.15 + Unacceptable broken system for Windows.

--- part12/todo-app/todo-frontend/vite.config.js
+++ part12/todo-app/todo-frontend/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom'
+  server: {
+    watch: {
+      usePolling: true
+    },
+    host: true,
+    hmr: {
+      host: 'localhost'
+    }
   }
 })
